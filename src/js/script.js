3a591e124e5be69dd82f8fd1d4a4a34f5c703216
PineCone-fromthewood <63791889+PineCone-fromthewood@users.noreply.github.com>
2021-04-19 16:42:11 +0200

    Delete src directory

--- src/js/script.js
+++
@@ -1,6 +0,0 @@
-const menuIcon = document.querySelector(".hamburger-menu");
-const sidenav = document.querySelector(".sidenav");
-
-menuIcon.addEventListener("click", () => {
-  sidenav.classList.toggle("change");
-});
